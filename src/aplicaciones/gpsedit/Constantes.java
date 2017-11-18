3e445f2bcd8e5daaacb38b617c016b60fdc1d82d
jdemiguel <32634934+jdemiguel@users.noreply.github.com>
2017-11-18 16:28:51 +0100

    up

--- src/aplicaciones/gpsedit/Constantes.java
+++
@@ -1,7 +0,0 @@
-package aplicaciones.gpsedit;
-
-
-public class Constantes {
-	public static enum EjeDominio {DISTANCIA, TIEMPO_MOVIMIENTO, TIEMPO_ABSOLUTO, HORA};
-	public static enum triggerMethod {Manual, Distance, Location, Time, HeartRate};
-}
--- src/aplicaciones/gpsedit/excepciones/ExtensionInvalidaException.java
+++
@@ -1,9 +0,0 @@
-package aplicaciones.gpsedit.excepciones;
-
-public class ExtensionInvalidaException extends Exception {
-	private static final long serialVersionUID = 2700567944916607144L;
-	
-	public ExtensionInvalidaException(String message)  {
-		super (message);
-	}
-}
